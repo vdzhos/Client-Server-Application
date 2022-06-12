9f9b77b060bba952114563ea26ff60f03c81d633
--replace-all <dgosan37@gmail.com>
2022-06-12 16:40:13 +0300

    Added 2 tests

--- src/test/java/EncodeDecodeTests.java
+++ src/test/java/EncodeDecodeTests.java
@@ -4,6 +4,8 @@ import objects.Packet;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 public class EncodeDecodeTests {
 
     @Test
@@ -27,8 +29,28 @@ public class EncodeDecodeTests {
     }
 
     @Test
-    void testDecodeFailure() {
-        byte[] text = "Failure message".getBytes();
+    void sameEncodingOfTwoSamePacketsSuccess(){
+        byte[] text = "some text".getBytes();
+
+        Message message1 = new Message(Command.UPDATE,1, text);
+        Packet packet1 = new Packet((byte) 1,1L, message1);
+
+        Message message2 = new Message(Command.UPDATE,1, text);
+        Packet packet2 = new Packet((byte) 1,1L, message2);
+
+        try {
+            byte[] encoded1 = PacketProcessor.encode(packet1);
+            byte[] encoded2 = PacketProcessor.encode(packet2);
+            Assertions.assertTrue(Arrays.equals(encoded1,encoded2));
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assertions.fail();
+        }
+    }
+
+    @Test
+    void testDecodeHeaderChecksumFailure() {
+        byte[] text = "header checksum failure".getBytes();
 
         Message message = new Message(Command.UPDATE,1, text);
         Packet initialPacket = new Packet((byte) 1,1L, message);
@@ -37,6 +59,26 @@ public class EncodeDecodeTests {
             byte[] encodedPacket = PacketProcessor.encode(initialPacket);
             encodedPacket[14] = 0x00;
             encodedPacket[15] = 0x00;
+            PacketProcessor.decode(encodedPacket);
+            // incorrect crc
+            Assertions.fail();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            Assertions.assertTrue(true);
+        }
+    }
+
+    @Test
+    void testDecodeMessageChecksumFailure() {
+        byte[] text = "message checksum failure".getBytes();
+
+        Message message = new Message(Command.DELETE,5, text);
+        Packet initialPacket = new Packet((byte) 5,5L, message);
+
+        try {
+            byte[] encodedPacket = PacketProcessor.encode(initialPacket);
+            encodedPacket[encodedPacket.length-2] = 0x00;
+            encodedPacket[encodedPacket.length-1] = 0x00;
             PacketProcessor.decode(encodedPacket);
             // incorrect crc
             Assertions.fail();
